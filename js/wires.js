074fc8536230421bacdea2f5f331f5f441ed0616
tamtrant3 <tamtran0112@gmail.com>
2022-08-05 23:07:50 +1000

    updated js

---
+++ js/wires.js
@@ -0,0 +1,38 @@
+$('.burger').click(function() {
+    $('.menu').slideToggle()
+})
+
+ScrollReveal().reveal('.casestudysection', {
+    delay: 300,
+    distance: '0px',
+    duration: 1000,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    interval: 0,
+    opacity: 0,
+    origin: 'bottom',
+    rotate: {
+        x: 0,
+        y: 0,
+        z: 0,
+    },
+    scale: 0.9,
+    cleanup: true,
+    container: document.documentElement,
+    desktop: true,
+    mobile: true,
+    reset: true,
+    useDelay: 'always',
+    viewFactor: 0.0,
+    viewOffset: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+    },
+    afterReset: function (el) {},
+    afterReveal: function (el) {
+        ScrollReveal().clean(el);
+    },
+    beforeReset: function (el) {},
+    beforeReveal: function (el) {},
+}); 
